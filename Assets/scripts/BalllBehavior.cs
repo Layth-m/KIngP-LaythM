94e438f7245f657b2c6fd4cfc1cbd6e18cdcfb26
Layth-m <laythmaloul2021@gmail.com>
2025-01-27 15:52:53 -0600

    Add pin movement
    Added pin behavior script that moves the pin with the movement of the mouse cursor.

--- Assets/scripts/BalllBehavior.cs
+++ Assets/scripts/BalllBehavior.cs
@@ -17,10 +17,14 @@ public class BalllBehavior : MonoBehaviour
     public float maxLaunchSpeed;
     public float minTimeToLaunch; 
     public float maxTimeToLaunch;
-    public float cooldown;
+    private float cooldown;
+        
     public bool launching;
-    public float launchDuration;
+    private float launchDuration;
+        
     public float timeLastLaunch;
+
+    public float timeLaunchStart;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,20 +34,56 @@ public class BalllBehavior : MonoBehaviour
      //   maxSpeed = 15.0f;
 
       targetPosition =  getRandomPosition();
-        
-        
+        cooldown = Random.Range(2, 10);
+        launchDuration = Random.Range(3, 15);
     }
 
     // Update is called once per frame
     void Update()
     {
+       if(onCooldown() == false)
+        {
+            if(launching == true)
+            {
+                float currentLaunchTime = Time.time - timeLaunchStart;
+                if(currentLaunchTime > launchDuration)
+                {
+                    startCooldown();
+                }
+                else
+                {
+                    launch();
+                }
+            }
+         
+        }
 
         Vector2 currentPos = gameObject.GetComponent<Transform>().position;
+        //currentPos = transform.position;
         float distance = Vector2.Distance(currentPos, targetPosition);
 
         if(distance > 0.1) {
             float difficulty = getDifficultypercentage();
-            float currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
+            float currentSpeed;
+            if (launching == true)
+            {
+                float launchingForHowLong = Time.time - timeLaunchStart;
+                if (launchingForHowLong < launchDuration)
+                {
+                    launch();
+                }
+                else 
+                { 
+                    startCooldown();
+                }
+
+                currentSpeed = Mathf.Lerp(minLaunchSpeed, maxLaunchSpeed, difficulty);
+            }
+            else
+            {
+                currentSpeed = Mathf.Lerp(minLaunchSpeed, maxLaunchSpeed, difficulty);
+            }
+           
             currentSpeed = currentSpeed * Time.deltaTime;
             Vector2 newPosition = Vector2.MoveTowards(currentPos,targetPosition,currentSpeed);
             transform.position = newPosition;
@@ -51,10 +91,27 @@ public class BalllBehavior : MonoBehaviour
         }
         else
         {
-            targetPosition = getRandomPosition();
+            if(launching == true)
+            {
+                startCooldown();
+            }
+
+           targetPosition = getRandomPosition();
         }
 
-        //getRandomPosition();
+
+
+      // float timeLaunching = Time.time - timeLastLaunch;
+      // if(timeLaunching > launchDuration)
+      //  {
+      //      startCooldown();
+      //  }
+      //  else
+      //  {
+      //      launch();
+      //  }
+       
+
     }
 
     Vector2 getRandomPosition()
@@ -62,9 +119,7 @@ public class BalllBehavior : MonoBehaviour
         float randomX = Random.Range(minX, maxX);
         float randomY = Random.Range(minY, maxY);
         Vector2 v = new Vector2(randomX,randomY);
-
         return v;
-
     }
 
 
@@ -81,7 +136,34 @@ public class BalllBehavior : MonoBehaviour
 
     public void launch()
     {
+        targetPosition = target.transform.position;
 
+        if (launching == false)
+        {
+            timeLaunchStart = Time.time;
+            launching = true;
+
+        }
+
+    }
+
+    public bool onCooldown()
+    {
+        bool result = false;
+        float timeSinceLastLaunch = Time.time - timeLastLaunch;
+
+        if(timeSinceLastLaunch < cooldown)
+        {
+            result = true;
+        }
+        return result; 
+    }
+
+    public void startCooldown()
+    {
+        launching = false;
+        timeLastLaunch = Time.time;
+       
     }
 
 
