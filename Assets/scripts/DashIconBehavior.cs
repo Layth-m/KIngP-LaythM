2b29aea6e6763bf1e9a9c0b893a5f6879dea2d1b
Layth-m <laythmaloul2021@gmail.com>
2025-02-10 19:50:24 -0600

    Add main menu scene.

--- Assets/scripts/DashIconBehavior.cs
+++ Assets/scripts/DashIconBehavior.cs
@@ -1,22 +1,37 @@
 using UnityEngine;
-using TMPro; 
+using TMPro;
+using UnityEngine.UI;
 
 public class DashIconBehavior : MonoBehaviour
 {
     TextMeshProUGUI label;
     float cooldown;
-
+    public Image overlay;
     float cooldownRate;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
         label = GetComponentInChildren<TextMeshProUGUI>();
+        Image[] images = GetComponentsInChildren<Image>();
+        for(int i = 0; i<images.Length; i++)
+        {
+            if (images[i].tag == "overlay")
+            {
+                overlay = images[i];
+            }
+        }
+        cooldownRate = PinBehavior.cooldownRate;
+        overlay.fillAmount = 6.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        cooldown = PinBehavior.cooldown;
         string message = " ";
         if(PinBehavior.cooldown> 0.0)
         {
