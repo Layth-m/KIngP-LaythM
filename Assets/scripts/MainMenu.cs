79fe488fdde79101bad5a2513079c711641a3f2a
Layth-m <laythmaloul2021@gmail.com>
2025-02-21 22:59:52 -0600

    Add Audio Pt1.
    Started implementing game audio, and added more scene transition buttons.

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -1,28 +1,44 @@
 using UnityEngine;
+using System.Collections;
+
+
+using System.Collections.Generic; 
 
 public class Main_menu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public void gotoCharacterSelection()
     {
-        
+
+        StartCoroutine(WaitForSoundTransition("CharacterSelection"));
+        UnityEngine.SceneManagement.SceneManager.LoadScene("CharacterSelection");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
     public void gotoGame()
 
+    {
 
-    { 
+        StartCoroutine(WaitForSoundTransition("MainGame"));
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainGame");
 
     
     }
+
+    public void gotoMainMenu()
+    {
+        StartCoroutine(WaitForSoundTransition("MainMenu"));
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    private IEnumerator WaitForSoundTransition(string sceneName)
+    {
+        AudioSource audioSource = GetComponentInChildren<AudioSource>();
+        audioSource.Play();
+
+        yield return new WaitForSeconds(audioSource.clip.length);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+
 
 
 
